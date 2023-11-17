a4c701ad74f3257746815d13b47ff10ad4620a22
Sheila <sheilarojas17@gmail.com>
2023-11-17 10:39:43 +0100

    nh

--- code.js
+++ code.js
@@ -3,40 +3,42 @@ const SWAPI_BASE_URL = 'https://wizard-world-api.herokuapp.com';
 window.onload = async()=> {
     const btn = document.getElementById("btn_magos");
     const wizards = await getAllMagos();
-    const wrapper = document.getElementById('wrapper_magos');
+  
         
 
-        //for (const wizard of wizards ) {
+        for (const wizard of wizards ) {
+            const wrapper = document.getElementById('wrapper_magos');
             const newElement = document.createElement('div');
+            const elixirs=wizard.elixirs;
+        
+            for(const elixir of wizard.elixirs){
+
+                newElement.innerHTML= `
+                <h2>${wizard.lastName}</h2> 
+                <p>${elixir.name}</p>`
+                wrapper.appendChild(newElement);
+            }
+           
             
-    
-            newElement.innerHTML = `
-             
-              <p>${wizards.lastName}</p>
-            `;
-
-            wrapper.appendChild(newElement);
-
-            // console.log(wrapper);
-       // }
+        }
 
 }
 
 async function getAllMagos() {
     const response = await fetch(`${SWAPI_BASE_URL}/wizards`);
-    
-    for(let i =0; i<response; i++){
-        const data = await response.json();
-        console.log(data);
-        return data[i];
-    }
-        
+    const data = await response.json();
+   
+   return data;
+}
+// async function getAllElixirs(){
+//     const response = await fetch(`${SWAPI_BASE_URL}/wizards`);
+// }
 
    // console.log(data.elixirs);
  //   return data.;
 
 
-}
+
 
 
 
