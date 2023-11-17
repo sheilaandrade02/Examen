520e010d5c585245e88abedf0742c95780d6ce68
Sheila <sheilarojas17@gmail.com>
2023-11-17 09:29:38 +0100

    hola

--- code.js
+++ code.js
@@ -3,9 +3,9 @@ const SWAPI_BASE_URL = ' https://wizard-world-api.herokuapp.com ';
 window.onload = async()=>{
     const btn = document.getElementById("btn_magos");
 
-    btn.addEventListener('click', handleAddHTML);
+   // btn.addEventListener('click', handleAddHTML);
 
-    async function handleAddHTML() {
+   // async function handleAddHTML() {
         const wizards = await getAllMagos();
         const wrapper = document.getElementById('wrapper_magos');
 
@@ -13,17 +13,22 @@ window.onload = async()=>{
             const newElement = document.createElement('div');
 
             newElement.innerHTML = `
-              <h2>${wizard.title}</h2>
-              <p>${wizard.opening_crawl}</p>
+              <h2>${wizard.firstName}</h2>
+              <p>${wizard.lastName}</p>
             `;
 
             wrapper.appendChild(newElement);
+
+            // console.log(wrapper);
         }
-    }
+   // }
 }
 
 async function getAllMagos() {
     const response = await fetch(`${SWAPI_BASE_URL}/wizards`);
     const data = await response.json();
-    return data.elixirs;
+    for(data=0; )
+   // console.log(data.elixirs);
+    return data.;
+
 }
