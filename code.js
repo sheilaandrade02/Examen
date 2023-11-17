283eddb33d131b13b732e203442bd8986bbf785a
Sheila <sheilarojas17@gmail.com>
2023-11-17 11:09:49 +0100

    hj

--- code.js
+++ code.js
@@ -2,25 +2,43 @@ const SWAPI_BASE_URL = 'https://wizard-world-api.herokuapp.com';
 
 window.onload = async()=> {
     const btn = document.getElementById("btn_magos");
-    const wizards = await getAllMagos();
-  
-        
+    const btn_e=document.getElementById("btn_elixir");
+    btn.addEventListener('click',handleHTML);
 
-        for (const wizard of wizards ) {
-            const wrapper = document.getElementById('wrapper_magos');
+    async function handleHTML(){
+        const wizards = await getAllMagos();
+        const wrapper = document.getElementById('wrapper_magos');
+
+        for (const wizard of wizards ) {  
             const newElement = document.createElement('div');
-            const elixirs=wizard.elixirs;
-        
             for(const elixir of wizard.elixirs){
-
-                newElement.innerHTML= `
+                
+                newElement.innerHTML= (`beforebegin`,`
                 <h2>${wizard.lastName}</h2> 
-                <p>${elixir.name}</p>`
+                <p>${elixir.name}</p>`)
                 wrapper.appendChild(newElement);
+                
+               
             }
            
-            
         }
+    }
+
+    btn_e.addEventListener('click',mostrarIngredientes);
+    async function mostrarIngredientes(){
+        const elixirs=await getAllIngredients();
+        const texto= document.getElementById('wrapper_elixir');
+
+        for(const elixir of elixirs){
+            const newElement = document.createElement('div');
+            for(const ingredient of elixirs.ingredients){
+                newElement.innerHTML = (`
+                <h2>${elixir.name}</h2>
+                <p>${ingredient.name}</p>`)
+                texto.appendChild(newElement);
+            }
+        }
+    }
 
 }
 
@@ -29,6 +47,13 @@ async function getAllMagos() {
     const data = await response.json();
    
    return data;
+}
+
+async function getAllIngredients(){
+    const response = await fetch(`${SWAPI_BASE_URL}/elixirs`);
+    const data =await response.json();
+    return data;
+
 }
 // async function getAllElixirs(){
 //     const response = await fetch(`${SWAPI_BASE_URL}/wizards`);
